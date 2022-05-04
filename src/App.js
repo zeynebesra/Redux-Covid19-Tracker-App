c4ac5136d38607e6ded756a1a6bcf1057a1713a7
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-05-04 21:56:30 +0300

    homework end

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import CardArea from "./components/CardArea";
+import Dropdown from "./components/Dropdown";
+import { Image, Flex } from "@chakra-ui/react";
+import Graphic from "./components/Graphic";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchAsyncData } from "./redux/dataSlice";
+import { Container } from "@chakra-ui/react";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { country, loading } = useSelector((state) => state.data);
+
+  useEffect(() => {
+    dispatch(fetchAsyncData());
+  }, [country, dispatch]);
+
+  return (
+    <Container maxW={"container.lg"} mb={20}>
+      <Flex justifyContent={"center"} my={8}>
+        <Image src="https://world19covid.web.app/static/media/image.d7265326.png" />
+      </Flex>
+      <CardArea />
+      <Dropdown />
+      {!loading && <Graphic />}
+    </Container>
+  );
+};
+
+export default App;
